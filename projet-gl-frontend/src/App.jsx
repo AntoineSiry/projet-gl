1ce1177c588edef0e37f06bb5ab54732e0e5f929
AntoineSiry <78871104+AntoineSiry@users.noreply.github.com>
2025-06-25 19:04:08 +0200

    Init frontend with tailwind and vite

---
+++ projet-gl-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-4xl/loose font-bold text-white">
+        Projet GL - Frontend (avec Tailwind v4)
+      </h1>
+    </div>
+  )
+}
+
+export default App
